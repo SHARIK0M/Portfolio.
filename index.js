a79b4376f91188343e452366544b7562b80411d5
SHARIK0M <sharik.x.af@gmail.com>
2024-09-14 12:57:54 +0530

    final

--- index.js
+++ index.js
@@ -7,3 +7,28 @@ function opn(){
 function clo(){
     navbar.style.display="none"
 }
+
+
+function sendemail(){
+
+    var email=document.getElementById("email").value;
+    var from_name=document.getElementById("name").value;
+    var message=document.getElementById("message").value;
+    
+    var templateParams = {
+        email: email,
+        to_name: name,
+        message: message
+      };
+    
+      emailjs.send('service_q9wmthv', 'template_xims95r', templateParams)
+    .then(function(response) {
+     console.log('SUCCESS!', response.status, response.text);
+     window.alert("Sent successfully!");
+     
+    })
+    
+     
+    }
+                
+     
